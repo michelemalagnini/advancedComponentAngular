fe902bc4a130af45f2e5325607b4703925169688
michele malagnini <michelemalagn@gmail.com>
2023-11-27 11:26:07 +0100

    WIP: created a component with dinamic content with ng-content

--- advancedComponent/src/app/app.component.ts
+++ advancedComponent/src/app/app.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
-
+import { BlogPostComponent } from './blog-post/blog-post.component';
 @Component({
   selector: 'app-root',
-  template: `<h1>Angular advanced component ---</h1>`,
+  template: ` <app-blog-post
+    title="Angular advanced component"
+    author="J.R. Lansdale"
+    date="01/11/202221"
+  >
+    <div class="blog-post-content">
+      <p>
+        Siamo in texas è un venerdi sera e l'Orbit è stipato di gente che
+        sgomita per pop corn e Coca-cola, pregustando la Grande notte Horro
+      </p>
+    </div>
+    <div class="blog-post-sidebar">
+      <h2>blog posts</h2>
+      <ul>
+        <li>uno</li>
+        <li>due</li>
+        <li>tre</li>
+        <li>quattro</li>
+      </ul>
+    </div>
+  </app-blog-post>`,
   styleUrls: [],
 })
-export class AppComponent {
-  title = 'advancedComponent';
-}
+export class AppComponent {}
---
+++ advancedComponent/src/app/blog-post/blog-post.component.ts
@@ -0,0 +1,19 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  standalone: true,
+  selector: 'app-blog-post',
+  template: `<div class="blog-post">
+    <h2>{{ title }}</h2>
+    <p class="author">{{ author }}</p>
+    <p class="date">{{ date }}</p>
+    <ng-content select=".blog-post-content"></ng-content>
+    <ng-content select=".blog-post-sidebar"></ng-content>
+  </div>`,
+  styleUrls: [],
+})
+export class BlogPostComponent {
+  @Input() title: string | undefined;
+  @Input() author: string | undefined;
+  @Input() date: string | undefined;
+}
